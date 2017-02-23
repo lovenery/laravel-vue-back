2fb988e5255799d94ca89e943dea15130ba03b8f
Hsu <edittye379@gmail.com>
2017-02-23 18:10:43 +0800

    Deleting Products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -16,4 +16,13 @@ class ProductsController extends Controller
         $product = Product::create($request->all());
         return $product;
     }
+    public function destroy($id)
+    {
+        try {
+            Product::destroy($id);
+            return response([], 204);
+        } catch (\Exception $e) {
+            return response(['Problem deleting the product', 500]);
+        }
+    }
 }
